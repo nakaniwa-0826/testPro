7aabad87f289d2accfa12f3857b26326b346b971
nakaniwa <hno30826mika@gmail.com>
2024-04-18 22:41:21 +0900

    Changes

---
+++ Flower/app/src/main/java/com/example/flower/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.flower
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 画面上部メッセージ
+        val messageView: TextView = findViewById(R.id.messageView)
+        // 花の画像イメージ
+        val flowerImage: ImageView = findViewById(R.id.flowerImage)
+        // 水をあげるボタン
+        val waterBtn: Button = findViewById(R.id.waterBtn)
+        // リセットボタン
+        val resetBtn: Button = findViewById(R.id.resetBtn)
+
+        // 水をあげるボタンクリックカウント
+        var count = 0
+        // 水を上げるボタンクリックイベント
+        waterBtn.setOnClickListener{
+            count++
+            // messageView.text = "$count"
+            // クリックカウントで表示メッセージを分岐
+            when (count) {
+                in 1 .. 19 -> {
+                    messageView.text = getString(R.string.message0)
+                    flowerImage.setImageResource(R.drawable.f0)
+                }
+                in 20 .. 39 -> {
+                    messageView.text = getString(R.string.message1)
+                    flowerImage.setImageResource(R.drawable.f1)
+                }
+                in 40 .. 59 -> {
+                    messageView.text = getString(R.string.message2)
+                    flowerImage.setImageResource(R.drawable.f2)
+                }
+                in 60 .. 79 -> {
+                    messageView.text = getString(R.string.message3)
+                    flowerImage.setImageResource(R.drawable.f3)
+                }
+                in 80 .. 99 -> {
+                    messageView.text = getString(R.string.message4)
+                    flowerImage.setImageResource(R.drawable.f4)
+                }
+                else -> {
+                    messageView.text = getString(R.string.message5)
+                    flowerImage.setImageResource(R.drawable.f5)
+                    waterBtn.visibility = View.INVISIBLE
+                    resetBtn.visibility = View.VISIBLE
+                }
+            }
+
+            // リセットボタンクリックイベント
+            resetBtn.setOnClickListener{
+                count = 0
+                messageView.text = getString(R.string.message)
+                flowerImage.setImageResource(R.drawable.f0)
+                waterBtn.visibility = View.VISIBLE
+                resetBtn.visibility = View.INVISIBLE
+            }
+        }
+    }
+}
